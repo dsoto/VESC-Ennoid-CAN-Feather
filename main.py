a8622e6e5da146f8ee5731ddc168473cd85b5d9e
Daniel Soto <sotod@sonoma.edu>
2021-05-23 08:44:35 -0700

    adds variables to TFT output

--- main.py
+++ main.py
@@ -46,12 +46,13 @@ derived_data = {'internal_resistance':0.090,
                 'charge':0,
                 'energy':0,
                 'trip_efficiency':0,
-                'instantaneous_efficiency':0}
+                'instantaneous_efficiency':0,
+                'time_delta':0}
 
 vehicle_parameters = {'wheel_circumference':1.89}
 
 strings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-strings = [''] * 12
+strings = [''] * 16
 
 class DERIVED:
     def __init__(self):
@@ -61,14 +62,19 @@ class DERIVED:
                                ('low_cell_voltage',     vehicle_data, 'Vl', 3),
                                ('speed',     derived_data, 'S',   1),
                                ('motor_rpm', vehicle_data, 'rpm', 0),
-                                ('high_battery_temp', vehicle_data, 'Tbat', 1),
-                                ('high_BMS_temp', vehicle_data, 'Tbms', 1),
-                        ('battery_voltage_BMS', vehicle_data, 'Vb', 1),
-                        ('battery_current',     vehicle_data, 'Ic', 1),
-                        ('battery_current_BMS', vehicle_data, 'Ib', 1),
-                        ('motor_current',       vehicle_data, 'Im', 1),
-                        ('controller_temperature', vehicle_data, 'Tc', 1),
-                        ('motor_temperature', vehicle_data, 'Tm', 1)]
+                               ('high_battery_temp', vehicle_data, 'Tbat', 1),
+                               ('high_BMS_temp', vehicle_data, 'Tbms', 1),
+                               ('battery_voltage_BMS', vehicle_data, 'Vb', 1),
+                               ('battery_current',     vehicle_data, 'Ic', 1),
+                               ('battery_current_BMS', vehicle_data, 'Ib', 1),
+                               ('motor_current',       vehicle_data, 'Im', 1),
+                               ('controller_temperature', vehicle_data, 'Tc', 1),
+                               ('motor_temperature', vehicle_data, 'Tm', 1),
+                               ('distance', derived_data, 'km', 1),
+                               ('energy', derived_data, 'Wh', 1),
+                               ('charge', derived_data, 'Ah', 1),
+                               ('time_delta', derived_data, 'dt', 3)]
+
 
     def update(self, ready, strings):
         if ready == True:
@@ -80,6 +86,7 @@ class DERIVED:
             derived_data['distance'] += derived_data['speed'] * time_delta / 1000.0
             derived_data['energy'] += vehicle_data['battery_current'] * vehicle_data['battery_voltage'] * time_delta / 3600.0
             derived_data['charge'] += vehicle_data['battery_current'] * time_delta / 3600.0
+            derived_data['time_delta'] = time_delta
             self.make_strings(strings)
         return False
 
@@ -457,12 +464,17 @@ class TFT_3:
                             label.Label(font, color=color, max_glyphs=20, x=10, y=y_offset + 4 * y_spacing),
                             label.Label(font, color=color, max_glyphs=20, x=10, y=y_offset + 5 * y_spacing),
                             label.Label(font, color=color, max_glyphs=20, x=10, y=y_offset + 6 * y_spacing),
-                            label.Label(font, color=color, max_glyphs=20, x=200, y=y_offset),
-                            label.Label(font, color=color, max_glyphs=20, x=200, y=y_offset + 1 * y_spacing),
-                            label.Label(font, color=color, max_glyphs=20, x=200, y=y_offset + 2 * y_spacing),
-                            label.Label(font, color=color, max_glyphs=20, x=200, y=y_offset + 3 * y_spacing)]
+                            label.Label(font, color=color, max_glyphs=20, x=10, y=y_offset + 7 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 1 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 2 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 3 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 4 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 5 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 6 * y_spacing),
+                            label.Label(font, color=color, max_glyphs=20, x=240, y=y_offset + 7 * y_spacing)]
 
-        self.text_group = displayio.Group(max_size=12)
+        self.text_group = displayio.Group(max_size=16)
         for tl in self.text_labels:
             self.text_group.append(tl)
         self.display.show(self.text_group)
@@ -474,11 +486,11 @@ class TFT_3:
             self.text_group[self.update_line].text = strings[self.update_string]
 
             self.update_line += 1
-            if self.update_line > 10:
+            if self.update_line > 15:
                 self.update_line = 0
 
             self.update_string += 1
-            if self.update_string > 10:
+            if self.update_string > 15:
                 self.update_string = 0
 
             self.display.show(self.text_group)
